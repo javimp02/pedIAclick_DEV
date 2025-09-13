c5591cb36d7a62687378b4248e1583126ec11c4f
javimp02 <202411137@alu.comillas.edu>
2025-09-13 13:17:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ BRAVE_TOKEN = st.secrets.get("BRAVE_TOKEN", "")
 OPENAI_KEY = st.secrets.get("OPENAI_API_KEY", "")
 OPENAI_KEY_IMAGES = st.secrets.get("OPENAI_KEY_IMAGES", "")
 
+st.write(f"🔐 Clave de imágenes cargada: {OPENAI_KEY_IMAGES[:5]}...")  # Solo muestra los primeros caracteres
+
 client = OpenAI(api_key=OPENAI_KEY)
 client_images = OpenAI(api_key=OPENAI_KEY_IMAGES)
 
