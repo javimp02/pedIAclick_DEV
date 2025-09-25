ca2d56cf342d1119529a7c8ab4eaa9dd8dcf7b53
javimp02 <202411137@alu.comillas.edu>
2025-09-25 17:55:42 +0200

    Create app.py

---
+++ imagenes/app.py
@@ -0,0 +1,21 @@
+def generar_imagen_dalle(prompt_img: str):
+    try:
+        with open("assets/referencia.jpeg", "rb") as f:
+            response = client_images.images.edit(
+            model="gpt-image-1",
+            image=f,
+            prompt=prompt_img,
+            size="1024x1024"
+            )
+            
+        image_obj = response.data[0]
+        if image_obj.url:
+            return image_obj.url
+        elif image_obj.b64_json:
+            return base64.b64decode(image_obj.b64_json)
+        else:
+            st.error("⚠️ La API no devolvió URL ni imagen base64.")
+            return None
+    except Exception as e:
+        st.error(f"⚠️ Error al generar la imagen: {e}")
+        return None
